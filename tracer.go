bafe8b5e73c1352cb87d2086da787c2d3418ccc8
youthtrouble <47859940+youthtrouble@users.noreply.github.com>
2020-07-08 15:27:28 +0100

    v 1.1

--- tracer.go
+++ tracer.go
@@ -1,13 +1,25 @@
 package trace
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 //Tracer is the interface
 type Tracer interface {
 	Trace(...interface{})
 }
 
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
 //New func
 func New(w io.Writer) Tracer {
-	return nil
+	return &tracer{out: w}
 }
